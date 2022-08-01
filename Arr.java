b4551ff3b2972dc5cf047484b9c4a4908fe25b76
AkarshiKulshrestha <97840852+AkarshiKulshrestha@users.noreply.github.com>
2022-08-01 14:22:18 +0530

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,17 @@
+package codes;
+
+public class Arr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] arr = {"Ajsj","Bjsj","Cjsj","Dkjskj","Ejsj"};
+		//for(int i=0;i<=arr.length-1;i++) {
+			//System.out.println(arr[i]);
+		//}
+		for(String s: arr) {
+			System.out.println(s);
+		}
+
+	}
+
+}
