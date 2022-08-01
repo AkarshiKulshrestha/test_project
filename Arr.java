637f1150348ed04e5613b67e59dbfb24201b9d45
AkarshiKulshrestha <97840852+AkarshiKulshrestha@users.noreply.github.com>
2022-08-01 15:11:23 +0530

    Update Arr.java

--- Arr.java
+++ Arr.java
@@ -4,7 +4,7 @@ public class Arr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] arr = {"Ajsj","Bjsj","Cjsj","Dkjskj","Ejsj"};
+		String[] arr = {"Ajsj","Bjsj","Cjsj","Dkjskj","Ejsj","jsahkjsa"};
 		//for(int i=0;i<=arr.length-1;i++) {
 			//System.out.println(arr[i]);
 		//}
